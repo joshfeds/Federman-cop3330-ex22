04d9e4d2281b41c10ef7f6c138334347e85bd29b
joshfeds14 <joshfeds14@gmail,com>
2021-06-05 18:59:44 -0400

    Exercise 22

---
+++ src/main/java/oop/exercise/Exercise22.java
@@ -0,0 +1,33 @@
+package oop.exercise;
+import java.util.Scanner;
+/*
+ *  UCF COP3330 Summer 2021 Assignment 1 Solution
+ *  Copyright 2021 Joshua Federman
+ */
+public class Exercise22
+{
+    static Scanner input = new Scanner(System.in);
+
+    public static void main( String[] args ) {
+        int number1;
+        int number2;
+        int number3;
+        int max = Integer.MIN_VALUE;
+
+        System.out.print("Enter the first number: ");
+        number1 = input.nextInt();
+        System.out.print("Enter the second number: ");
+        number2 = input.nextInt();
+        System.out.print("Enter the third number: ");
+        number3 = input.nextInt();
+
+        if(number1 > max)
+            max = number1;
+        if(number2 > max)
+            max = number2;
+        if(number3 > max)
+            max = number3;
+
+        System.out.print("The largest number is " + max);
+    }
+}
